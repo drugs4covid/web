4e59119fa98bdad2c93bb3f4b87fcc2cb11e830f
vfaranda <victorfla.personal@gmail.com>
2022-09-13 12:40:13 +0200

    Project structure

---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="home">
+    <b-container>
+      <b-jumbotron header="BootstrapVue" lead="Bootstrap v4 Components for Vue.js 2">
+        <p>For more information visit our website</p>
+        <b-btn variant="primary" href="https://bootstrap-vue.org/">More Info</b-btn>
+      </b-jumbotron>
+
+      <b-form-group
+          horizontal
+          :label-cols="4"
+          description="Let us know your name."
+          label="Enter your name"
+      >
+        <b-form-input v-model.trim="name"></b-form-input>
+      </b-form-group>
+
+      <b-alert variant="success" :show="showAlert">Hello {{ name }}</b-alert>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  components: {
+
+  }
+}
+</script>
