830d567e23588006e6f55622124d3d04b6d067cb
vfaranda <victorfla.personal@gmail.com>
2022-09-20 16:18:22 +0200

    Added i18n for multi-language support

--- src/main.js
+++ src/main.js
@@ -7,11 +7,13 @@ import router from './router'
 import {BootstrapVueIcons} from "bootstrap-vue";
 import axios from "axios";
 import VueAxios from 'vue-axios'
+import i18n from './i18n'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVueIcons, axios, VueAxios)
 
 new Vue({
   router,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false,
+      enableBridge: false
+    }
+  }
 })
