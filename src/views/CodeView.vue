5a30126c8e2d51de77639f02109a0c2f7ee5efc4
vfaranda <victorfla.personal@gmail.com>
2022-10-06 14:23:25 +0200

    Issue #7: Custom bibliography

--- src/views/CodeView.vue
+++ src/views/CodeView.vue
@@ -4,70 +4,58 @@
       <v-card-title v-text="$t('code.title')"/>
       <v-card-subtitle v-text="$t('code.subtitle')"/>
 
-      <v-row>
-        <v-col v-for="(cite, index) in citationsFormated"
-               :key="index"
-               cols="12"
-        >
-          <span v-html="cite"/>
-        </v-col>
-      </v-row>
+      <v-col v-for="(cite, index) in citationsData"
+             :key="index"
+             cols="12"
+      >
+        <a :href="cite.URL">
+          <h2 class="title" v-text="cite.title">
+            {{cite.title}}
+          </h2>
+          <span v-show="cite.version">
+            ({{ $t('label.version') }} {{cite.version}})
+          </span>
+        </a>
+        <h3 class="subtitle-2">
+          <span v-for="(author, index) in cite.author"
+                :key="index"
+          >
+            {{author.given}} {{author.family}},
+          </span>
+        </h3>
+        <v-card-text v-text="cite.abstract"/>
 
-      <div class="csl-bib-body">
-        <div data-csl-entry-id="https://doi.org/10.5281/zenodo.1005176" class="csl-entry">Willighagen, L., & Willighagen, E. (2017, October 9). Larsgw/Citation.Js V0.3.3. Zenodo. https://doi.org/10.5281/zenodo.1005176</div>
-      </div>
-
-<!--      <li v-for="(entry, index) in citationList"-->
-<!--          :key="index"-->
-<!--      >-->
-<!--                        <span v-if="entry.available">-->
-<!--                            <a :href="entry.available" target="_blank">-->
-<!--                                <span style="text-decoration: underline;" class="title">{{ entry.title }}</span>,-->
-<!--                            </a>-->
-<!--                        </span>-->
-<!--        <span v-else-if="entry.FILE">-->
-<!--                             <a :href="paper_pool + entry.FILE" target="_blank">-->
-<!--                                <span style="text-decoration: underline;" class="title">{{ entry.title }}</span>,-->
-<!--                            </a>-->
-<!--                        </span>-->
-<!--        <span v-else>-->
-<!--                            <span style="text-decoration: underline;" class="title">{{ entry.title }}</span>,-->
-<!--                        </span>-->
-<!--        <div class="author">-->
-<!--                            <span v-if="entry.author">-->
-<!--                                {{ entry.author }}-->
-<!--                            </span>-->
-<!--        </div>-->
-<!--        <div>-->
-<!--          <span v-if="entry.journal"><em><span class="journal">{{ entry.journal }}</span></em>,</span>-->
-<!--          <span v-else-if="entry.bookTitle"><em><span class="journal">{{ entry.bookTitle }}</span></em>,</span>-->
-<!--          <span v-if="entry.location"><span class="location">{{ entry.location }}</span>,</span>-->
-<!--          <span v-if="entry.volume"><span class="volume">vol. {{ entry.volume }}</span>,</span>-->
-<!--          <span v-if="entry.number"><span class="number">no. {{ entry.number }}</span>,</span>-->
-<!--          <span v-if="entry.pages"><span class="pages">pp. {{ entry.pages }}</span>,</span>-->
-<!--          <span v-if="entry.month"><span class="month">{{ entry.month }}</span>,</span>-->
-<!--          <span v-if="entry.year"><span class="year">{{ entry.year }}</span>.</span>-->
-<!--          <span v-else>.</span>-->
-
-<!--          <a role="button" data-toggle="collapse" :href="'#' + entry.BIBTEXKEY + '-bib'" aria-expanded="false" :aria-controls="entry.BIBTEXKEY + '-bib'" >-->
-<!--            [bib]-->
-<!--          </a>-->
-<!--        </div>-->
-<!--        <div v-if="entry.keywords">-->
-<!--          <span class="badge badge-success" v-for="(keyword) in entry.keywords"-->
-<!--                :key="keyword"-->
-<!--          >-->
-<!--            {{ keyword }}-->
-<!--          </span>-->
-<!--        </div>-->
-<!--        <div v-else-if="entry.label">-->
-<!--          <span  class="badge badge-success" v-for="keyword in entry.label"-->
-<!--                 :key="keyword"-->
-<!--          >-->
-<!--            {{ keyword }}-->
-<!--          </span>-->
-<!--        </div>-->
-<!--      </li>-->
+        <v-card-actions>
+          <v-row>
+            <v-col cols="12">
+              <span v-show="cite.publisher">
+                <em><span class="journal" v-text="cite.publisher"/></em>
+              </span>
+              <span v-if="cite.publisher && cite.issued">, </span>
+              <span v-if="cite.publisher && !cite.issued">. </span>
+              <span v-show="cite.issued">
+                <span v-for="(datePart, index) in cite.issued['date-parts']"
+                      :key="index"
+                >
+                  <span v-show="datePart[1]" v-text="$t('month.' + datePart[1])"/>
+                  <span v-if="datePart[1]">, </span>
+                  <span v-show="datePart[0]" v-text="datePart[0]"/>.
+                </span>
+              </span>
+            </v-col>
+            <v-col cols="12">
+              <v-chip-group column>
+                <v-chip v-for="(category) in new Set(cite.categories)"
+                        :key="category"
+                        v-text="category"
+                        color="success"
+                        label small
+                />
+              </v-chip-group>
+            </v-col>
+          </v-row>
+        </v-card-actions>
+      </v-col>
 
     </v-card>
   </v-container>
@@ -79,22 +67,21 @@ import Cite from 'citation-js';
 // More info about Cite input formats in:
 // https://citation.js.org/api/0.3/tutorial-input_formats.html
 const CITE_INPUT=[
-    "https://doi.org/10.48550/arXiv.2012.01953",
-    "10.48550/arXiv.2012.01953",
-    "https://doi.org/10.48550/arXiv.2208.01093",
-    "10.48550/arXiv.2208.01093",
-    "@ARTICLE{smit54,AUTHOR = {J. G. Smith and H. K. Weston},TITLE = {Nothing Particular in this Year's History},YEAR = {1954},JOURNAL = {J. Geophys. Res.},VOLUME = {2},PAGES = {14-15}}"
+  "https://doi.org/10.48550/arXiv.2012.01953",
+  "10.48550/arXiv.2012.01953",
+  "https://doi.org/10.48550/arXiv.2208.01093",
+  "10.48550/arXiv.2208.01093",
+  "@ARTICLE{smit54,AUTHOR = {J. G. Smith and H. K. Weston},TITLE = {Nothing Particular in this Year's History},YEAR = {1954},JOURNAL = {J. Geophys. Res.},VOLUME = {2},PAGES = {14-15}}",
+  "@book{ANTLR,author = {Terence Parr},edition = {First},interhash = {d9ef4ed82183b86b6a3004161de5ea44},intrahash = {1688029f4c14bd3b234933a48e902c03},publisher = {Pragmatic Bookshelf},series = {Pragmatic Programmers},title = {The Definitive ANTLR Reference: Building Domain-Specific Languages},url = {http://www.amazon.com/Definitive-ANTLR-Reference-Domain-Specific-Programmers/dp/0978739256%3FSubscriptionId%3D13CT5CVB80YFWJEPWS02%26tag%3Dws%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3D0978739256},year = 2007,ean = {9780978739256},keywords = {Me:MastersThesis antlr compilers languages lexers parsers programming},asin = {0978739256},description = {Amazon.com: The Definitive ANTLR Reference: Building Domain-Specific Languages (Pragmatic Programmers): Terence Parr: Books},isbn = {0978739256},biburl = {http://www.bibsonomy.org/bibtex/21688029f4c14bd3b234933a48e902c03/gron},dewey = {005.45},month = May}",
 ]
 
 export default {
   name: "CodeView",
   computed: {
-    citationsList() {
-      return CITE_INPUT.map(item => {
-        return new Cite(item).data
-      })
+    citationsData() {
+      return new Cite(CITE_INPUT).data
     },
-    citationsFormated (){
+    citationsFormated() {
       return CITE_INPUT.map(item => {
         return new Cite(item).format('bibliography', {
           format: 'html',
