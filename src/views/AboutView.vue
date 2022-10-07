0b6185590f649c11d434d26f94c179db11d5e29e
vfaranda <victorfla.personal@gmail.com>
2022-10-07 15:46:10 +0200

    Issue #33: Replaced map img with google maps embedding

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -18,9 +18,16 @@
           </v-col>
         </v-row>
 
+      <br/><br/><v-divider/>
+
       <v-card-title v-text="$t('about.title')"/>
       <v-card-subtitle v-html="$t('about.subtitle')"/>
-      <v-img src="@/assets/planofi.jpg"/>
+      <v-col cols="12" flat>
+        <iframe class="h_iframe" id="iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3038.173372904003!2d-3.841707684344045!3d40.405009879366546!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd4185f258e5eec5%3A0xdf7b007bb6904fa5!2sEscuela%20T%C3%A9cnica%20Superior%20de%20Ingenieros%20Inform%C3%A1ticos%20de%20la%20Universidad%20Polit%C3%A9cnica%20de%20Madrid!5e0!3m2!1ses!2ses!4v1665149738222!5m2!1ses!2ses"
+                allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade"
+        />
+      </v-col>
+
     </v-card>
   </v-container>
 </template>
@@ -84,5 +91,16 @@ export default {
 </script>
 
 <style scoped>
-
+#iframe{
+  height: 600px;
+  border: 0;
+}
+.h_iframe iframe {
+  width: 100%;
+  height: 100%;
+}
+.h_iframe {
+  height: 100%;
+  width: 100%;
+}
 </style>
