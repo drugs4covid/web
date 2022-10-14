2f846263d055c9366a1a28af9422626228835ac5
vfaranda <victorfla.personal@gmail.com>
2022-10-14 15:31:38 +0200

    Organized storage system

--- src/store/index.js
+++ src/store/index.js
@@ -5,58 +5,87 @@ Vue.use(Vuex)
 
 const GITHUB_BASE_URL = "https://github.com/"
 const LIBRAIRY_BASE_URL = "https://librairy.linkeddata.es/"
-const SOLR_BASE_URL = "https://librairy.linkeddata.es/solr/"
 const HUGGINGFACE_BASE_URL = "https://huggingface.co/"
 const D4C_BASE_URL = "https://drugs4covid.oeg.fi.upm.es/"
 
 export default new Vuex.Store({
   state: {
-    bioNer: {
-      github: GITHUB_BASE_URL.concat("drugs4covid/bio-ner"),
-      api: LIBRAIRY_BASE_URL.concat("bio-ner/entities"),
-      huggingFace: HUGGINGFACE_BASE_URL.concat("alvaroalon2")
+    links: {
+      github: {
+        d4c: GITHUB_BASE_URL.concat("drugs4covid"),
+        bioNer: GITHUB_BASE_URL.concat("drugs4covid/bio-ner"),
+        bioNlp: GITHUB_BASE_URL.concat("drugs4covid/bio-nlp"),
+        bioQa: GITHUB_BASE_URL.concat("drugs4covid/KGQA"),
+        rdf: GITHUB_BASE_URL.concat("drugs4covid/EBOCA-Resources"),
+        sparql: GITHUB_BASE_URL.concat("drugs4covid/KG"),
+        notebooks: {
+          cord19_indexing: GITHUB_BASE_URL.concat("drugs4covid/bio-nlp/blob/master/notebooks/documents/cord19_indexing.ipynb"),
+          atc_processing: GITHUB_BASE_URL.concat("drugs4covid/bio-nlp/blob/master/notebooks/drugs/atc_processing.ipynb"),
+          mesh_processing: GITHUB_BASE_URL.concat("drugs4covid/bio-nlp/blob/master/notebooks/diseases/mesh_processing.ipynb")
+        }
+      },
+      librairy: {
+        bioNerEntities: LIBRAIRY_BASE_URL.concat("bio-ner/entities"),
+        covid19Model: LIBRAIRY_BASE_URL.concat("covid19-model"),
+        bioApi: LIBRAIRY_BASE_URL.concat("bio-api"),
+        solrApi: LIBRAIRY_BASE_URL.concat("solr/"),
+        solr: {
+          disease: LIBRAIRY_BASE_URL.concat("solr/bioner-diseases/select?q=*%3A*"),
+          drugs: LIBRAIRY_BASE_URL.concat("solr/bioner-drugs/select?q=*%3A*"),
+          genes: LIBRAIRY_BASE_URL.concat("solr/bioner-genetic/select?q=*%3A*"),
+          papers: LIBRAIRY_BASE_URL.concat("solr/cord19-papers/select?q=*%3A*"),
+          paragraphs: LIBRAIRY_BASE_URL.concat("solr/cord19-paragraphs/select?q=*%3A*"),
+        },
+      },
+      huggingFace: {
+        bioBert: HUGGINGFACE_BASE_URL.concat("alvaroalon2")
+      },
+      d4c: {
+        qaAnswers: D4C_BASE_URL.concat("qa/answers"),
+        rdf: D4C_BASE_URL.concat("rdf"),
+        sparql: D4C_BASE_URL.concat("sparql"),
+        ontology  : "https://drugs4covid.github.io/EBOCA-portal/index.html",
+        keyQ: "https://aiproc.linkeddata.es/",
+        search: "https://search.drugs4covid.oeg-upm.net/search/",
+      },
+      zenodo: {
+        annotatedCorpus: null
+      },
     },
-    bioSearch: {
-      github: GITHUB_BASE_URL.concat("drugs4covid/bio-nlp"),
-    },
-    bioQa: {
-      github: GITHUB_BASE_URL.concat("drugs4covid/KGQA"),
-      api: "https://d4c.linkeddata.es/qa/answers"
-    },
-    rdf: {
-      github: GITHUB_BASE_URL.concat("drugs4covid/EBOCA-Resources"),
-      url: D4C_BASE_URL.concat("rdf")
-    },
-    sparql: {
-      github: GITHUB_BASE_URL.concat("drugs4covid/KG"),
-      url: D4C_BASE_URL.concat("sparql")
-    },
-    textSearch: {
-      github: GITHUB_BASE_URL.concat("drugs4covid/bio-nlp"),
-      librairy: LIBRAIRY_BASE_URL.concat("covid19-model")
-    },
-    repository: {
-      solrDisease: SOLR_BASE_URL.concat("bioner-diseases/select?q=*%3A*"),
-      solrDrugs: SOLR_BASE_URL.concat("bioner-drugs/select?q=*%3A*"),
-      solrGenes: SOLR_BASE_URL.concat("bioner-genetic/select?q=*%3A*"),
-      solrPapers: SOLR_BASE_URL.concat("cord19-papers/select?q=*%3A*"),
-      solrParagraphs: SOLR_BASE_URL.concat("cord19-paragraphs/select?q=*%3A*"),
-    },
-    d4c: {
 
-    },
-    externalLink: { // For externalLink view. Must be refactored to use the other states
-      sparql: "https://drugs4covid.oeg.fi.upm.es/sparql",
-      rdf: "https://drugs4covid.oeg.fi.upm.es/rdf/",
-      ontology  : "https://drugs4covid.github.io/EBOCA-portal/index.html",
-      repository: "http://librairy.linkeddata.es/solr/#/cord19-paragraphs/query",
-      keyQ: "https://aiproc.linkeddata.es/",
-      search: "https://search.drugs4covid.oeg-upm.net/search/",
-    },
-    api: {
-      solr: "https://librairy.linkeddata.es/solr/",
-      bioApi: "https://librairy.linkeddata.es/bio-api/"
-    },
+    citedDocuments: [
+      /*ID 1: Cannot be cited, there is no .bib or doi reference */
+      /*ID 2*/"https://oa.upm.es/cgi/export/eprint/67933/BibTeX/upm-eprint-67933.bib",
+      /*ID 3*/"@article{PLN6437,author = {Carlos Badenes-Olmedo y Álvaro Alonso y Oscar Corcho},title = {An Overview of Drugs, Diseases, Genes and Proteins in the CORD-19 Corpus},journal = {Procesamiento del Lenguaje Natural},volume = {69},number = {0},year = {2022},keywords = {},abstract = {Several initiatives have emerged during the COVID-19 pandemic to gather scientific publications related to coronaviruses. Among them, the COVID-19 Open Research Dataset (CORD-19) has proven to be a valuable resource that provides full-text articles from the PubMed Central, bioRxiv and medRxiv repositories. Such a large amount of biomedical literature needs to be properly managed to facilitate and promote its use by health professionals, for example by tagging documents with the biomedical entities that appear on them. We created a biomedical named entity recognizer (NER) that normalizes (NEN) the drugs, diseases, genes and proteins mentioned in texts with the codes of the main standardization systems such as MeSH, ICD-10, ATC, SNOMED, ChEBI, GARD and NCBI. It is based on fine-tuning the BioBERT language model independently for each entity type using domain-specific datasets and an inverse index search to normalize the references. We have used the resultant BioNER+BioNEN system to process the CORD-19 corpus and offer an overview of the drugs, diseases, genes and proteins related to coronaviruses in the last fifty years.},issn = {1989-7553},url = {http://journal.sepln.org/sepln/ojs/ojs/index.php/pln/article/view/6437},pages = {165--176}}",
+      /*ID 4: Cannot be cited, there is no .bib or doi reference */
+      /*ID 5*/"https://oa.upm.es/cgi/export/eprint/71455/BibTeX/upm-eprint-71455.bib",
+      /*ID 6*/"https://oa.upm.es/cgi/export/eprint/71427/BibTeX/upm-eprint-71427.bib",
+      /*ID 7: Cannot be cited, there is no .bib or doi reference */
+      /*ID 8: Cannot be cited, there is no .bib or doi reference */
+      /*ID 9*/"https://oa.upm.es/cgi/export/eprint/70949/BibTeX/upm-eprint-70949.bib",
+      /*ID 10*/"https://oa.upm.es/cgi/export/eprint/67901/BibTeX/upm-eprint-67901.bib",
+      /*ID 11: Cannot be cited, No content */
+      /*ID 12: Cannot be cited, No content */
+      /*ID 13*/"10.48550/arXiv.2208.01093",
+      /*ID 13*/"10.1007/978-3-031-17105-5_1",
+      /*ID 13*/"10.1007/978-3-031-17105-5_2",
+      /*ID 13*/"10.1007/978-3-031-17105-5_3",
+      /*ID 13*/"10.1007/978-3-031-17105-5_4",
+      /*ID 13*/"10.1007/978-3-031-17105-5_5",
+      /*ID 13*/"10.1007/978-3-031-17105-5_6",
+      /*ID 13*/"10.1007/978-3-031-17105-5_7",
+      /*ID 13*/"10.1007/978-3-031-17105-5_8",
+      /*ID 13*/"10.1007/978-3-031-17105-5_9",
+      /*ID 13*/"10.1007/978-3-031-17105-5_10",
+      /*ID 14*/"10.1007/978-3-031-17105-5_11",
+      /*ID 13*/"10.1007/978-3-031-17105-5_12",
+      /*ID 13*/"10.1007/978-3-031-17105-5_13",
+      /*ID 13*/"10.1007/978-3-031-17105-5_14",
+      /*ID 13*/"10.1007/978-3-031-17105-5_15",
+      /*ID 13*/"10.1007/978-3-031-17105-5_16",
+      /*Example*/"https://doi.org/10.48550/arXiv.2012.01953",
+      /*Example*/"10.48550/arXiv.2208.01093",
+    ],
   },
   getters: {
   },
