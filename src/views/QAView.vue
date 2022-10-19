d15ae717cb0b8d631d86c3f13baccf6621e23eb8
vfaranda <victorfla.personal@gmail.com>
2022-10-19 17:51:33 +0200

    Validation for qa View

--- src/views/QAView.vue
+++ src/views/QAView.vue
@@ -5,63 +5,75 @@
       <v-card-subtitle v-text="$t('qaView.subtitle')"/>
 
       <v-container>
-        <v-row justify="center" align-content="center">
+        <v-form ref="form">
 
-          <v-col cols="11">
-            <v-text-field v-model="input"
-                          :placeholder="$t('qaView.placeholder')"
-                          outlined hide-details
-            >
-            </v-text-field>
-          </v-col>
+          <!-- QA Input -->
+          <v-row>
+            <v-col cols="12">
+              <v-text-field v-model="input"
+                            :placeholder="$t('qaView.placeholder')"
+                            :rules="rules.inputText"
+                            outlined
+              >
+                <template #append-outer>
+                  <v-btn height="56px" width="56px"
+                         class="search-btn"
+                         :loading="loading"
+                         outlined
+                         @click="clickAskHandler()"
+                  >
+                    <v-icon size="xx-large">mdi-chat-question-outline</v-icon>
+                  </v-btn>
+                </template>
+              </v-text-field>
+            </v-col>
+          </v-row>
 
-          <v-col cols="1">
-            <v-btn height="56px"
-                   :loading="loading"
-                   outlined
-                   @click="askQuestion(input, maxAnswers)"
+          <!-- Max Answers -->
+          <v-row>
+            <v-subheader class="subheader" v-text="$t('qaView.subheader1')+':'"/>
+            <v-slider v-model="maxAnswers"
+                      class="align-center"
+                      max="25" min="1"
+                      hide-details
             >
-              <v-icon size="xx-large">mdi-chat-question-outline</v-icon>
-            </v-btn>
-          </v-col>
-
-          <v-col cols="12">
-            <v-slider
-                v-model="maxAnswers"
-                class="align-center"
-                max="25"
-                min="1"
-                hide-details
-                :label="$t('qaView.maxAnswers')"
-            >
-              <template v-slot:append>
-                <v-text-field
-                    v-model="maxAnswers"
-                    class="mt-0 pt-0"
-                    hide-details
-                    single-line
-                    type="number"
-                    style="width: 60px"
-                ></v-text-field>
+              <template #prepend>
+                <v-text-field v-model="maxAnswers"
+                              :rules="rules.maxAnswers"
+                              class="mt-0 pt-0 align-center"
+                              single-line outlined dense hide-details
+                              type="number"
+                              style="width: 100px"
+                />
               </template>
             </v-slider>
-          </v-col>
+          </v-row>
 
-          <v-col>
-            <v-row justify="center" align-content="center">
-              <v-checkbox v-model="useD4C" label="D4c"
-                          hide-details
-              />
-              <v-checkbox v-model="useDBPedia" label="DB Pedia"
-                          hide-details hide-spin-buttons
-              />
-              <v-checkbox v-model="useWiki" label="Wikidata"
-                          hide-details hide-spin-buttons
-              />
-            </v-row>
-          </v-col>
+          <!-- Source Checkboxes -->
+          <v-row >
+            <v-subheader class="subheader" v-text="$t('qaView.subheader2')+':'"/>
 
-        </v-row>
+            <v-checkbox v-model="useD4C"
+                        class="filter-checkbox"
+                        :label="$t('qaView.d4c')"
+                        :rules="rules.resource"
+                        hide-spin-buttons
+            />
+            <v-checkbox v-model="useDBPedia"
+                        class="filter-checkbox"
+                        :label="$t('qaView.dbPedia')"
+                        hide-spin-buttons
+                        :rules="rules.resource"
+            />
+            <v-checkbox v-model="useWiki"
+                        class="filter-checkbox"
+                        :label="$t('qaView.wikidata')"
+                        hide-spin-buttons
+                        :rules="rules.resource"
+            />
+          </v-row>
+
+        </v-form>
       </v-container>
 
       <q-a-card v-for="(answer, index) in answerList"
@@ -94,17 +106,40 @@ export default {
     input: "What traditional medicine has been used to treat Covid-19?",
     question: "",
     maxAnswers: 3,
-    useWiki: false,
-    useDBPedia: false,
+    useWiki: true,
+    useDBPedia: true,
     useD4C: true,
-    loading: false
+    loading: false,
   }),
+  computed: {
+   rules() {
+      return {
+        inputText: [
+          v => !!v || this.$t("error.validation.required"),
+        ],
+        maxAnswers: [
+          v => (1 <= v && v <= 5) || this.$t("error.validation.outOfRange", {min: 1, max: 25}),
+        ],
+        resource: [
+          this.useWiki || this.useD4C || this.useDBPedia
+        ],
+      }
+    }
+  },
   methods:{
-    askQuestion(){
-      this.loading = true
-      this.question = this.input
+    clickAskHandler(){
+      if (!this.$refs.form.validate()) {
+        setTimeout(() => {this.$refs.form.resetValidation();}, 5000);
+      }
+      else{
+        this.loading = true
+        this.question = this.input
 
-      axiosService.qaAnswers(this.question, this.maxAnswers, this.useWiki, this.useDBPedia, this.useD4C)
+        this.askQuestion(this.question, this.maxAnswers, this.useWiki, this.useDBPedia, this.useD4C)
+      }
+   },
+    askQuestion(question, maxAnswers, useWiki, useDBPedia, useD4C){
+      axiosService.qaAnswers(question, maxAnswers, useWiki, useDBPedia, useD4C)
           .then(response => {
             this.answerList = response.data
           })
@@ -114,11 +149,24 @@ export default {
           .finally(() => {
             this.loading = false
           })
-    }
+    },
   },
+  mounted() {
+    this.useDBPedia = false
+    this.useWiki = false
+  }
 }
 </script>
 
 <style scoped>
-
+.subheader{
+  width: 145px;
+}
+.search-btn{
+  margin-top: -18px;
+}
+.filter-checkbox{
+  min-width: 200px;
+  margin-top: 8px;
+}
 </style>
