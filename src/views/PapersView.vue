3006e1fd0260ad22341b114bcd63d6e911e9a8ac
vfaranda <victorfla.personal@gmail.com>
2022-10-21 14:10:08 +0200

    Issue #35: Handsome papers <3

--- src/views/PapersView.vue
+++ src/views/PapersView.vue
@@ -1,31 +1,60 @@
 <template>
   <v-container>
     <v-card flat>
-      <v-skeleton-loader v-if="isLoading" type="article@5"/>
 
-      <v-card-text v-else v-for="(cite, index) in citationsList"
+      <v-card-text v-for="(cite, index) in citationsList"
                    :key="index"
       >
         <v-row>
-          <div>
-            <!-- Document ID -->
-            <span>[{{cite.id.split('_')[1]}}]{{' '}}</span>
-            <!-- Authors -->
-            <span>{{authorsToString(cite.author)}}.</span>
-            <!-- Title -->
-            <a :href="cite.URL" style="text-decoration: none">
-              <em>"{{cite.title}}"</em>
-            </a>
-          </div>
+
+          <v-col cols="11">
+            <div>
+              <!-- Document ID -->
+              <span>[{{cite.id.split('_')[1]}}] {{' '}}</span>
+
+              <!-- Authors -->
+              <span>{{authorsToString(cite.author)}}.</span>
+
+              <!-- Title -->
+              <span v-show="cite.title">
+                {{' '}}
+                <a v-if="cite.URL" :href="cite.URL" style="text-decoration: none">
+                  <em>"{{cite.title}}".</em>
+                </a>
+                <em v-else>"{{cite.title}}".</em>
+              </span>
+
+              <!-- Genre -->
+              <span v-show="cite.genre">
+                {{' '}} {{cite.genre}}.
+              </span>
+
+              <!-- Publisher -->
+              <span v-show="cite.publisher">
+                {{' '}} {{cite.publisher}}.
+              </span>
+
+              <!-- Book Info (title, vol, pages) -->
+              <span v-if="cite['container-title'] || cite.volume || cite.page">
+                {{' '}} {{bookInfoToString(cite['container-title'], cite.volume, cite.page)}}
+              </span>
+
+              <!-- Date -->
+              <span v-if="cite.issued">
+                {{' '}} {{dateToString(cite.issued['date-parts'])}}.
+              </span>
+
+            </div>
+          </v-col>
+
+          <v-col align-self="center">
+            <v-btn v-show="getDocPDF(cite.id)" :href="getDocPDF(cite.id)" icon plain>
+              <v-icon color="#B0321F">mdi-file-download</v-icon>
+            </v-btn>
+          </v-col>
+
         </v-row>
-
-        <v-row>
-          <v-btn v-show="getDocPDF(cite.id)" :href="getDocPDF(cite.id)" icon plain>
-            <v-icon color="#B0321F">mdi-file-download</v-icon>
-          </v-btn>
-        </v-row>
-
-
+        <v-divider/>
       </v-card-text>
 
     </v-card>
@@ -40,29 +69,38 @@ import BibFile from '@/static/cites.bib'
 // https://citation.js.org/api/0.3/tutorial-input_formats.html
 export default {
   name: "PapersView",
-  data: () => ({
-    citationsList: [],
-    isLoading: true,
-  }),
+  computed:{
+    citationsList() {
+      return new Cite(BibFile).data
+    }
+  },
   methods: {
-    parseBibtexFile(){
-      this.citationsList = new Cite(BibFile).data
-    },
     authorsToString(authorList) {
       return authorList.map(author => {
         return author.given + ' ' + author.family
       }).toString().replaceAll(',',", ")
     },
+    dateToString(dateParts){
+      return dateParts.map(date => {
+        switch (date.length){
+          case 1:
+            return date[0]
+          case 2:
+            return this.$t('month.' + date[1]) + ', ' + date[0]
+          default:
+            return date.toString()
+        }
+      }).toString()
+    },
+    bookInfoToString(title, vol, page) {
+      return [title, vol, page]
+          .filter(item => item)
+          .toString()
+          .replaceAll(',',', ') + '.'
+    },
     getDocPDF(citeId){
       return this.$store.state.pdfDocuments[citeId]
     }
-  },
-  mounted() {
-    this.isLoading = true
-    this.parseBibtexFile()
-    console.log(this.citationsList)
-
-    this.isLoading = false
   },
 }
 </script>
